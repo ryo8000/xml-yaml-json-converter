bdcf8c2b97999681d873bec902c4ef1e02f6747d
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2025-06-04 20:44:03 +0900

    Create handler

---
+++ src/handler.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: "OK",
+  };
+};
